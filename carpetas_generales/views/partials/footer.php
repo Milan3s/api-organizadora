105d9a07e1e4cd61d01baacd30d7453c7bc9739e
David Milanes Moreno <35001922+Milan3s@users.noreply.github.com>
2024-06-28 22:05:54 +0200

    subida de prueba
    o o o o o

--- carpetas_generales/views/partials/footer.php
+++
@@ -1,5 +0,0 @@
-    <script src="../../../framework/js/jquery-3.7.1.js"></script>
-    <script src="../../../framework/js/bootstrap.min.js"></script>
-    <script src="../../../framework/js/bootstrap.bundle.js"></script>
-</body>
-</html>
